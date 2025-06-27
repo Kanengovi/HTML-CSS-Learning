046f445f66e842e8d462381479bebfa388e6d167
KANE <152256135+Kanengovi@users.noreply.github.com>
2025-06-27 12:19:56 +0700

    Add files via upload

---
+++ Day_08/main.js
@@ -0,0 +1,21 @@
+function hello() {
+    alert("Hello World")
+}
+let a = 10;
+let b = 3;
+
+console.log("Tổng:", a + b);
+console.log("Hiệu:", a - b);
+console.log("Tích:", a * b);
+console.log("Thương:", a / b);
+console.log("Chia lấy dư:", a % b);
+
+console.log("a == b:", a == b );
+console.log("a === b:", a === b);
+console.log("a != b:", a != b);
+console.log("a !== b:", a !==b);
+console.log("a < b:", a < b);
+console.log("a > b:", a > b);
+console.log("a <= b:", a <= b);
+console.log("a >= b:", a >= b);
+
